c5ce3fa521f11363fbea5b7ef7af7bbaa52cfc86
Niklas Seppälä <nitasepp@student.jyu.fi>
2020-05-06 15:49:51 +0300

    Added endpoint for posting new banner.

--- banner_manager/banners.py
+++ banner_manager/banners.py
@@ -28,3 +28,55 @@ def banners():
 @bp.route('/')
 def index():
     return render_template('banners/index.html')
+
+
+@bp.route('/add_banner', methods=['POST'])
+def add_banner():
+    if request.method == 'POST':
+        banner_name = request.form['banner-name']
+        banner_code = request.form['banner-code']
+        banner_faction = request.form['banner-faction']
+        banner_image_name = None
+
+        if request.form['file-url']:
+            banner_image_name = download_from_url()
+
+        if banner_image_name and banner_faction and banner_code and banner_name:
+            db = get_db()
+            db.execute(
+                'INSERT INTO banner (banner_name, code, image_filename, faction) '
+                'VALUES (?,?,?,?)', (banner_name, banner_code, banner_image_name, banner_faction))
+            db.commit()
+            flash('Banner added.', category='success')
+    return redirect(url_for('banners.index'))
+
+
+def download_from_url():
+    url = request.form['file-url']
+    filename = urlparse(url).path.split('/')[-1]
+
+    # check file for illegal filetypes
+    if not allowed_file(filename):
+        flash('file not allowed.')
+        return None
+
+    filename = secure_filename(filename)  # make sure filename is safe
+    path = os.path.join(current_app.config['UPLOAD_FOLDER'], filename)
+    with current_app.open_instance_resource(path, 'wb') as f:
+        response = requests.get(url, stream=True)
+        if not response.ok:
+            flash("Couldn't download specified file.", 'error')
+            return None
+
+        # Write to file
+        for block in response.iter_content(1024):
+            if not block:
+                break
+            f.write(block)
+    return filename
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower(
+           ) in current_app.config["ALLOWED_EXTENSIONS"]
